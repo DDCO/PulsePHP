d354cc0c72872ed151870fbd920eaf2fe9fbb53e
DDCO <dan.da.cruz.oliveira@gmail.com>
2012-01-05 20:21:14 -0500

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ $_CONFIG["host"] = "localhost";
 $_CONFIG["driver"] = "mysql";
 $_CONFIG["db"] = "pulsephp";
 $_CONFIG["tablePrefix"] = "";
-$_CONFIG["username"] = "root";
-$_CONFIG["password"] = "038455820";
+$_CONFIG["username"] = "";
+$_CONFIG["password"] = "";
 /*End of Database Info*/
 $_CONFIG["template"] = "dreamy";
 $_CONFIG["SEO"] = true;
